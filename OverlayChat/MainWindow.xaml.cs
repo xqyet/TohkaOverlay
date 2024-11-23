94493fc580f7468cc0ad263566d5751f03b37942
giova <giovannib776@gmail.com>
2024-11-23 00:39:02 -0500

    Add project files.

---
+++ OverlayChat/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace OverlayChat
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PositionWindowBottomLeft();
+        }
+
+        private void PositionWindowBottomLeft()
+        {
+            // Set the window position to the bottom-left corner
+            this.Left = 0; // Align to the left of the screen
+            this.Top = SystemParameters.WorkArea.Height - this.Height; // Align to the bottom
+        }
+    }
+}
